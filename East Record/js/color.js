cfd500747405cce3cb4ce8e6e425a33c2af0c6ad
KeisukeHomma <ab13891012@gmail.com>
2016-07-02 10:34:27 +0900

    offcanvas

--- East Record/js/color.js
+++ East Record/js/color.js
@@ -1,10 +1,10 @@
-var col;
+var col = 0;
 
 window.onscroll = function(){
     var scrollTop =
             document.documentElement.scrollTop || // IE、Firefox、Opera
             document.body.scrollTop;              // Chrome、Safari
-    col = scrollTop/20 + 200;
+    col = -1 * scrollTop/10 + 200;
     console.log(col);
     document.body.style.background = "hsl(" + col + ", 70%, 45%)";
 }
